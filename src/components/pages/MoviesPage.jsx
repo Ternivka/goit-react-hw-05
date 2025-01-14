9cf2a1fe4e36c38b7b787bdd1f5a10bb5f9b77ea
rylrix <154704827+Ternivka@users.noreply.github.com>
2025-01-14 11:03:19 +0100

    Update MoviesPage.jsx

--- src/components/pages/MoviesPage.jsx
+++ src/components/pages/MoviesPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useSearchParams } from "react-router-dom";
-import MovieList from '../MovieList';
+import MovieList from "../MovieList/MovieList";
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
   const [query, setQuery] = useSearchParams();

e6e39185c2d8a9ac8d1f118a999a26ebb8bdf020
rylrix <154704827+Ternivka@users.noreply.github.com>
2025-01-14 11:28:59 +0100

    Update HomePage.jsx

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,15 +5,21 @@ import MovieList from "../MovieList/MovieList";
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
   const [category, setCategory] = useState("day");
+  const [error, setError] = useState(null);
+
+  const API_KEY = "af069d5a4aa6dab18750675f951f88b6";
+  const BASE_URL = "https://api.themoviedb.org/3/trending/movie/";
 
   const fetchTrendingMovies = async (timeframe) => {
-    const url = `https://api.themoviedb.org/3/trending/movie/${timeframe}?api_key=af069d5a4aa6dab18750675f951f88b6&language=en-US&page=1`;
+    const url = `${BASE_URL}${timeframe}?api_key=${API_KEY}&language=en-US&page=1`;
 
     try {
       const response = await axios.get(url);
       setMovies(response.data.results);
+      setError(null); // Reset error state when data is fetched successfully
     } catch (error) {
       console.error("Error fetching trending movies:", error);
+      setError("Failed to fetch movies. Please try again later.");
     }
   };
 
@@ -28,6 +34,9 @@ const HomePage = () => {
         <button onClick={() => setCategory("day")}>Trending Today</button>
         <button onClick={() => setCategory("week")}>Trending This Week</button>
       </div>
+
+      {error && <p>{error}</p>} {/* Display error if exists */}
+
       <MovieList movies={movies} />
     </div>
   );
